5f587f9c248d584c08c95f83fcdf7c5a33423967
Estela Saraí Cortés Córdova <aleetsee96@gmail.com>
2018-11-07 00:14:13 -0600

    Se agregaron funciones para insertar datos

--- application/models/insertar.php
+++ application/models/insertar.php
@@ -33,5 +33,52 @@ class Insertar extends CI_Model
 		return $this->db->replace($tabla, $data);
 	}
 
+	/*******************************************************
+	Agregar usuarios, ventas, configuraciones, productos
+	*******************************************************/
+	function newProveedor($data)
+	{
+		return $this->db->insert('tb_proveedores', $data);
+	}
+
+	function newProducto($data)
+	{
+		return $this->db->insert('tb_inventario', $data);
+	}
+	function setProducto($data,$where)
+	{
+		return $this->db->update('tb_inventario', $data,$where);
+	}
+
+	function newUser($data)
+	{
+		return $this->db->insert('tb_usuarios', $data);
+	}
+
+	function newDepartamento($data)
+	{
+		$this->db->insert('tb_departamentos', $data);
+		return $this->db->insert_id();
+	}
+
+
+	function setUser($data,$where)
+	{
+		return $this->db->update('tb_usuarios', $data,$where);
+	}
+	function newVenta($data)
+	{
+		$this->db->insert('tb_ventas', $data);
+		return $this->db->insert_id();
+	}
+
+
+
+	public function setConfig($data)
+	{
+		return $this->db->update('tb_config', $data);
+	}
+
+
 	}
 	?>
