0fe0d4aa70c00763b7ac66abb65cb4fc30ae6611
NATHANIEL007 <onehaki13@gmail.com>
2018-11-13 22:18:14 -0600

    Se agregan las funciones de eliminación de clientes, provvedores, usuaarios y productos

--- application/models/delete.php
+++ application/models/delete.php
@@ -7,17 +7,36 @@ class Delete extends CI_Model
     {
 		parent::__construct();
     }
-    //Eliminar un usuario de la tabla de usuarios :O
 
-   function delUser($id)
+
+    function delUser($id)
     {
         return $this->db->delete('tb_usuarios', array('id' => $id));
     }
-    //Eliminar una venta
-    function delMovimiento($idVenta,$idItem)
+
+		function delMovimientosFallidos($idVenta)
+    {
+        return $this->db->delete('tb_movimientosventas', array('idVenta' => $idVenta));
+    }
+
+		function delMovimiento($idVenta,$idItem)
     {
         return $this->db->delete('tb_movimientosventas', array('idVenta' => $idVenta,'idInventario'=>$idItem));
     }
 
+
+		function delDepartamento($id)
+    {
+        return $this->db->delete('tb_departamentos', array('id' => $id));
+    }
+
+		function delProveedor($id)
+    {
+        return $this->db->delete('tb_proveedores', array('id' => $id));
+    }
+		function delCliente($id)
+    {
+        return $this->db->delete('tb_clientes', array('id' => $id));
+    }
 }
 ?>
